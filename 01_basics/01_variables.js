4e383f97159674ac54cad39f437a5fa25b4ec3ea
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-07-03 08:28:15 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "divmish5@gmail.com"
+var accountPassword = "12345"
+accountCity =  "Jaipur"
+let accoutState;
+
+//   accountId =  2  // Not allowed
+
+accountEmail = "ashu393@gmail.com"
+accountPassword = "122434"
+accountCity = "Bengaluru"
+
+/*
+NOTES :-
+
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountCity, accountPassword, accoutState])
+
+ 
