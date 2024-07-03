bdd04344ea583aa3d7fb222fa2771a1a0ec7c978
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-07-03 13:44:36 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber)
+
+/* NOTES:-
+
+"33" => 33
+"33abc" => NaN (Not an Number)
+true => 1; false => 0
+
+*/
+
+let isLogggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLogggedIn)
+// console.log(booleanIsLoggedIn)
+
+/*
+NOTES:-
+
+1 => true; 0 => false
+"" => false
+"0" => true
+"divmish" => true
+
+*/
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+
+console.log(typeof stringNumber)
