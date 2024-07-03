f1f5919b3a618ac112826f2d331c05c935577c65
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-07-03 12:48:37 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    +3)  // code readability should be high
+
+console.log("divmish")
+
+let name= "Divakar"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standAlone value
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof Boolean)
