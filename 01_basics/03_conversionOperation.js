75d594650d5319c700424938945e6ce5afb04580
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-07-23 04:48:22 +0000

    Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,12 +1,10 @@
-let score = true
-
-console.log(typeof score);
-console.log(typeof(score))
+let score = true 
+// console.log(typeof score);
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 /* NOTES:-
 
@@ -17,9 +15,8 @@ true => 1; false => 0
 */
 
 let isLogggedIn = ""
-
-let booleanIsLoggedIn = Boolean(isLogggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLogggedIn)
+// console.log(booleanIsLoggedIn)
 
 /*
 NOTES:-
@@ -32,5 +29,42 @@ NOTES:-
 */
 
 let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+
+// ********************************* Operations ****************************
+
+let value = 3
+let negvalue = -value
+
+// console.log(negvalue);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2%3)
+// console.log(2*3);
+// console.log(2/3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2 
+console.log(str3)
+
+// consol e.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2 
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
