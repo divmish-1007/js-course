81dd408892a53d95b1cb5d1729546f627cff8670
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-08-22 10:34:36 +0000

    Basics of strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const name = "divakar"
+const repoCount = 4
+
+// console.log(name + repCount + " value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String("div-akar-mis-hra");
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(3));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.__proto__);
+
+const anotherString = gameName.slice(-7, 1);
+// console.log(anotherString);
+
+const newstringOne = "    divakar   "
+
+// console.log(newstringOne);
+// console.log(newstringOne.trim());
+
+const url = 'https:/hitesh.com/hitesh%20choudhary'
+
+// console.log(url.replace('%20' , '-'));
+
+// console.log(url.includes('sundar'));
+ 
+console.log(gameName.split('-'));
+
+
+
