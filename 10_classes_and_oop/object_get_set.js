307fae5c0013e6ecfbb2646f1c67783aa68bd78b
divmish-1007 <132588661+divmish-1007@users.noreply.github.com>
2024-09-24 08:35:25 +0000

    48 object Getter and setter Very Old method Raerly Use

---
+++ 10_classes_and_oop/object_get_set.js
@@ -0,0 +1,17 @@
+// This syntax use Rarely 
+
+const user = {
+    _email: 'h@hc.com',
+    _password: 'abc',
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value;
+    }
+}
+
+const tea = Object.create(user)
+console.log(tea.email)
